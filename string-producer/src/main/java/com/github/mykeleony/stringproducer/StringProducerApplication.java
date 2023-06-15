b455382955b8aa5dda8cb8e26a5347b7ff8742e9
Myke Amorim <mykeleony147@gmail.com>
2023-06-15 17:12:57 -0300

    Initial commit

---
+++ string-producer/src/main/java/com/github/mykeleony/stringproducer/StringProducerApplication.java
@@ -0,0 +1,13 @@
+package com.github.mykeleony.stringproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringProducerApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(StringProducerApplication.class, args);
+    }
+    
+}
---
+++ string-producer/src/test/java/com/github/mykeleony/stringproducer/StringProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mykeleony.stringproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringProducerApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
