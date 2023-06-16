85c08cc553c3516150bef8d0b6e27525b20280d7
Myke Amorim <mykeleony147@gmail.com>
2023-06-16 15:57:09 -0300

    Enabled kafka on application

--- string-consumer/src/main/java/com/github/mykeleony/stringconsumer/StringConsumerApplication.java
+++ string-consumer/src/main/java/com/github/mykeleony/stringconsumer/StringConsumerApplication.java
@@ -2,7 +2,9 @@ package com.github.mykeleony.stringconsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
+@EnableKafka
 @SpringBootApplication
 public class StringConsumerApplication {
     
