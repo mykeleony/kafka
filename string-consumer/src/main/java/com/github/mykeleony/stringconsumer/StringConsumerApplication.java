b58e35ced94d3b9c92d8a0c7a21a6c60e0cdc419
Myke Amorim <mykeleony147@gmail.com>
2023-06-16 14:59:06 -0300

    Basic modifications made
    Removed unnecessary 'static' and 'templates' folders. Changed application file extension to .yml

---
+++ string-consumer/src/main/java/com/github/mykeleony/stringconsumer/StringConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.github.mykeleony.stringconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringConsumerApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(StringConsumerApplication.class, args);
+    }
+    
+}
