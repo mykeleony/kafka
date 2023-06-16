e2e2e4748223dcd9d074a9edad43fb6e0f7b9bc4
Myke Amorim <mykeleony147@gmail.com>
2023-06-16 18:44:58 -0300

    Added topic and partition specification

--- string-consumer/src/main/java/com/github/mykeleony/stringconsumer/listener/StringConsumerListener.java
+++ string-consumer/src/main/java/com/github/mykeleony/stringconsumer/listener/StringConsumerListener.java
@@ -3,6 +3,7 @@ package com.github.mykeleony.stringconsumer.listener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,7 +11,9 @@ public class StringConsumerListener {
 
     private final Logger LOG = LoggerFactory.getLogger(StringConsumerListener.class);
     
-    @KafkaListener(groupId = "group-1", topics = "string-topic", containerFactory = "stringContainerFactory")
+    @KafkaListener(groupId = "group-1", topicPartitions = {
+            @TopicPartition(topic = "string-topic", partitions = {"0"})
+    }, containerFactory = "stringContainerFactory")
     public void create(String message) {
         LOG.info("Received message '{}'", message);
     }
