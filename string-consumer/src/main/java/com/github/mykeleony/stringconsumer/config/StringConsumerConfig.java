94ed06e20fa2ce9869495ab28b46e7a16e57becb
Myke Amorim <mykeleony147@gmail.com>
2023-06-16 21:38:05 -0300

    Added record interceptor config

--- string-consumer/src/main/java/com/github/mykeleony/stringconsumer/config/StringConsumerConfig.java
+++ string-consumer/src/main/java/com/github/mykeleony/stringconsumer/config/StringConsumerConfig.java
@@ -1,14 +1,16 @@
 package com.github.mykeleony.stringconsumer.config;
 
 import lombok.AllArgsConstructor;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.RecordInterceptor;
 
 import java.util.HashMap;
 
@@ -19,6 +21,7 @@ import static org.apache.kafka.clients.consumer.ConsumerConfig.*;
 public class StringConsumerConfig {
 
     private final KafkaProperties properties;
+    private final Logger LOG = LoggerFactory.getLogger(StringConsumerConfig.class);
     
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
@@ -39,5 +42,25 @@ public class StringConsumerConfig {
         
         return factory;
     }
-
+    
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> validateMessageContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        
+        factory.setConsumerFactory(consumerFactory());
+        factory.setRecordInterceptor(validateMessage());
+        
+        return factory;
+    }
+    
+    private RecordInterceptor<String, String> validateMessage() {
+        return (record, consumer) -> {
+            if (record.value().contains("Test")) {
+                LOG.info("Message contains 'test' word.");
+            }
+            
+            return record;
+        };
+    }
+    
 }
