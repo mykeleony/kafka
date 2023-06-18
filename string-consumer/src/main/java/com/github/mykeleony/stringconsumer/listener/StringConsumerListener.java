6a15c0b8940f809354ee4982504e1340745fafc3
Myke Amorim <mykeleony147@gmail.com>
2023-06-17 21:18:07 -0300

    Added exception code snippet to simulate error scenario

--- string-consumer/src/main/java/com/github/mykeleony/stringconsumer/listener/StringConsumerListener.java
+++ string-consumer/src/main/java/com/github/mykeleony/stringconsumer/listener/StringConsumerListener.java
@@ -1,10 +1,9 @@
 package com.github.mykeleony.stringconsumer.listener;
 
 import com.github.mykeleony.stringconsumer.listener.custom.StringConsumerCustomListener;
+import lombok.SneakyThrows;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,8 +12,13 @@ public class StringConsumerListener {
     private final Logger LOG = LoggerFactory.getLogger(StringConsumerListener.class);
     
     @StringConsumerCustomListener(groupId = "group-1")
+    @SneakyThrows
     public void create(String message) {
         LOG.info("Received message '{}'", message);
+        
+        if (message.equals("Exception")) {
+            throw new RuntimeException("Exception test!");
+        }
     }
 
 }
